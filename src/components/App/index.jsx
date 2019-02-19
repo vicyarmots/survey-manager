c6489151ea81a1a5aa98edd95370ac092da7439a
AntonProtas <anton.protasevich99@gmail.com>
2019-02-19 17:28:22 +0300

    optimize project

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,25 +3,9 @@ import Header from '../Header/index.jsx';
 
 import './index.css';
 
-const headerLogo = {
-  src: 'src/components/header/images/header-logo.png',
-  alt: 'header logo'
-};
-
-const navItems = [
-  {
-    link: '#',
-    label: 'О компании'
-  },
-  {
-    link: '#',
-    label: 'Вход'
-  }
-];
-
-class App extends Component {
+class App extends Component {;
   render() {
-    return <Header headerLogo={headerLogo} navItems={navItems} />;
+    return <Header/>;
   }
 }
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,22 +2,38 @@ import React, { Component } from 'react';
 
 import './index.css';
 
+const headerLogo = {
+  src: 'src/components/header/images/header-logo.png',
+  alt: 'header logo'
+};
+
+const navItems = [
+  {
+    link: '#',
+    label: 'О компании'
+  },
+  {
+    link: '#',
+    label: 'Вход'
+  }
+];
+
 class Header extends Component {
   render() {
-    console.log(this.props.navItems);
+    console.log();
     return (
       <header className="header">
         <div className="header__wrap-logo">
           <img
             className="header__wrap-logo__logo"
-            src={this.props.headerLogo.src}
-            alt={this.props.headerLogo.alt}
+            src={headerLogo.src}
+            alt={headerLogo.alt}
           />
         </div>
         <div className="header__wrap-nav">
           <nav className="header__wrap-nav__nav">
             <ul className="header__wrap-nav__nav__ul">
-              {this.props.navItems.map((item, index) => (
+              { !!navItems && navItems.map((item, index) => (
                 <li key={index}>
                   <a href={item.link}>{item.label}</a>
                 </li>
